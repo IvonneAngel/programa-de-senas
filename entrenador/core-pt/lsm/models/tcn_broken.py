7ca70aec6c0d5a3cc65c45dd6942ebf84715694b
Angel <angel@proyecto-lsm.com>
2026-09-04 02:16:14 -0600

    chore: fuera legacy muerto + gitignore blinda datos (raw/processed/whl)

--- entrenador/core-pt/lsm/models/tcn_broken.py
+++
@@ -1,6 +0,0 @@
-"""TCN - split en 5 partes (refactor). Importa desde split/* """
-from .split.tcn_part1 import *
-from .split.tcn_part2 import *
-from .split.tcn_part3 import *
-from .split.tcn_part4 import *
-from .split.tcn_part5 import *
